b7ed2ddd4cd806f3990943fda1f6cf4ccffdca0f
Maxim Averin <maks22rus120303@mail.ru>
2022-05-01 00:41:44 +0700

    Fixed

--- test/mytests.cpp
+++ test/mytests.cpp
@@ -1,9 +1,9 @@
 #include <ctest.h>
+#include <libgeometry/check_overlaps.h>
 #include <libgeometry/check_shape.h>
 #include <libgeometry/print.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <libgeometry/check_overlaps.h>
 
 CTEST(number, test1)
 {
@@ -23,22 +23,36 @@ CTEST(number, test3)
 CTEST(number, test4)
 {
     ASSERT_EQUAL(
-            0, check_triangle((char*)"triangle((1 2, 2 4, 4 2, 1 2))", (char*)"triangle"));
+            0,
+            check_triangle(
+                    (char*)"triangle((1 2, 2 4, 4 2, 1 2))",
+                    (char*)"triangle"));
 }
 
 CTEST(number, test5)
 {
-    ASSERT_EQUAL(0, check_triangle((char*)"triangle(1 2, 2 3, 3 4, 1 2)", (char*)"triangle"));
+    ASSERT_EQUAL(
+            0,
+            check_triangle(
+                    (char*)"triangle(1 2, 2 3, 3 4, 1 2)", (char*)"triangle"));
 }
 
 CTEST(number, test6)
 {
-    ASSERT_EQUAL(0, check_triangle((char*)"triangle((1 5, 2 3, 3 4, 1 6))", (char*)"triangle"));
+    ASSERT_EQUAL(
+            0,
+            check_triangle(
+                    (char*)"triangle((1 5, 2 3, 3 4, 1 6))",
+                    (char*)"triangle"));
 }
 
 CTEST(number, test7)
 {
-    ASSERT_EQUAL(0, check_triangle((char*)"triangle((1 5, 2 3, 3, 4,1 5))", (char*)"triangle"));
+    ASSERT_EQUAL(
+            0,
+            check_triangle(
+                    (char*)"triangle((1 5, 2 3, 3, 4,1 5))",
+                    (char*)"triangle"));
 }
 
 CTEST(number, test8)
